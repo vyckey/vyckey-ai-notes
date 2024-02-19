4ba52dbcaad7912fd10e06f9bfdb7a74d98344e1
vyckey <vyckey@qq.com>
2024-02-19 11:27:51 +0800

    add code

---
+++ code/start.py
@@ -0,0 +1,6 @@
+import network
+import mnist_loader
+
+training_data , validation_data , test_data = load_data_wrapper()
+net = Network([784, 30, 10])
+net.SGD(training_data, 30, 10, 3.0, test_data = test_data)
